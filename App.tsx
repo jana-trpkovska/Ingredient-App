4d6663b0f2b7b6b68065cdf84bd6f9f0054da6b5
Jana Trpkovska <jana.trpkovska@gmail.com>
2026-02-09 18:54:20 +0100

    Initial app setup

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ingredient App</Text>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 8,
+  },
+});
